ce76570dfddd390cc317e397fa6ccba77ef18a04
귀챠니즘 <yjm0003@naver.com>
2024-06-16 15:11:44 +0900

    Update annoyingChzzkMidAd.user.js

--- annoyingChzzkMidAd.user.js
+++ annoyingChzzkMidAd.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         Chzzk 중간광고 차단
 // @namespace    http://lazinism.woobi.co.kr/
-// @version      2024-06-16-01
+// @version      2024-06-16-02
 // @description  치지직 중간광고 차단
 // @author       귀챠니즘
 // @match        https://chzzk.naver.com/live/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=naver.com
 // @grant        GM_webRequest
-// @webRequest   {"selector":"https://api.chzzk.naver.com/ad-polling/v1/lives/*/ad*","action":"cancel"}
+// @webRequest   {"selector":"*api.chzzk.naver.com/ad-polling/v1/lives/*/ad*","action":"cancel"}
 // ==/UserScript==
 
 (function() {
     'use strict';
     GM_webRequest([
-        { selector: "https://api.chzzk.naver.com/ad-polling/v1/lives/*/ad?ts=*", action: 'cancel' },
+        { selector: "*api.chzzk.naver.com/ad-polling/v1/lives/*/ad*", action: 'cancel' },
     ], (info, message, details) => {
        console.log('중간광고 차단 시도.', info, message, details);
     });
