428d9ef2e3bca1047fc4338036d72aa69672c85f
귀챠니즘 <yjm0003@naver.com>
2024-11-29 20:28:35 +0900

    Update forceVisitedGray.user.js

--- forceVisitedGray.user.js
+++ forceVisitedGray.user.js
@@ -1,22 +1,14 @@
 // ==UserScript==
-// @name         Article Link Visited Color - Light Gray
-// @namespace    http://lazinism.woobi.com/
-// @version      1.0
-// @description  네이버 카페 이미본거 회색으로
-// @author       Lazinism
-// @match        https://cafe.naver.com/*
+// @name         네이버 카페 방문 페이지 회색표시하기
+// @namespace    http://nokduro.com/
+// @version      2024-11-29-00
+// @description  '이미 방문한 페이지'를 회색(#ccc)으로 표시합니다. 해당 색상은 '트게더'에 사용되었던 방문 페이지 색상과 동일합니다.
+// @author       귀챠니즘
+// @match        https://cafe.naver.com/MyCafeIntro.nhn*
+// @match        https://cafe.naver.com/ArticleList.nhn*
+// @match        https://cafe.naver.com/ca-fe/cafes/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=naver.com
+// @run-at       document-start
 // @grant        none
 // ==/UserScript==
-
-(function() {
-    'use strict';
-
-    // CSS 스타일을 추가하여 방문한 링크를 밝은 회색으로 설정
-    const style = document.createElement('style');
-    style.textContent = `
-        a.article:visited {
-            color: #b3b3b3 !important;
-        }
-    `;
-    document.head.appendChild(style);
-})();
+document.head.innerHTML+=`<style>a.article:visited,a.tit:visited,a.m-tcol-c:visited{color:#ccc !important;}</style>`;
